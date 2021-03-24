013272ec0d5a1794d86e5736031ae9777081f2f7
Sachin Wattamwar <sachin.w2000@gmail.com>
2021-03-24 13:36:17 +0530

    Added color for the output text

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const franc = require('franc');
 const langs = require('langs');
+const colors = require('colors');
 
 const input = process.argv[2];
 const string2 = franc(input); 
 
 if(string2 === 'und')
 {
-    console.log("Could not find the language, Try with different and more sample tests")
+    console.log("Could not find the language, Try with different and more sample tests".red)
 }
 else 
 {
 const Language = langs.where("3" , string2);
-console.log(Language.name);
+console.log(`Our best guess is : ${Language.name}`.random);
 }
